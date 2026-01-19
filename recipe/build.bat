936a084cfbb529f06ca0326732d1533dce88b81c
Dylan Madisetti <dylan@marimo.io>
2026-01-19 05:45:02 -0800

    add menu item to definition

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,3 +5,13 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 
 "%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
 IF %ERRORLEVEL% NEQ 0 exit 1
+
+:: menu (menuinst)
+mkdir "%PREFIX%\Menu"
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+"%PYTHON%" -c "import os, re; open(os.path.join(os.environ['PREFIX'], 'Menu', 'marimo-menu.json'), 'w').write(re.sub('__PKG_VERSION__', os.environ['PKG_VERSION'], open(os.path.join(os.environ['RECIPE_DIR'], 'menu.json')).read()))"
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+copy "%RECIPE_DIR%\marimo.ico" "%PREFIX%\Menu\"
+IF %ERRORLEVEL% NEQ 0 exit 1
